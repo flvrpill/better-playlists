993dd3ce1664c76fce94d54527da80a4e6681f69
flvrpill <65322208+flvrpill@users.noreply.github.com>
2020-09-04 14:35:39 -0500

    Cha cha cha changes....

--- src/App.js
+++ src/App.js
@@ -3,15 +3,41 @@ import './App.css';
 
 let defaultStyle = {
   color: "#fff"
+}
+let fakeServerData = {
+  user: {
+    name: 'Ryan',
+    playlists: [
+      {
+        name: 'My Favorites',
+        songs: ['City Girl', 'Fire and Bone', 'Boning Fire']
+      },
+      {
+        name: 'Dad Stuff',
+        songs: ['City Girl', 'Fire and Bone', 'Boning Fire']
+      },
+      {
+        name: 'Kid Songs',
+        songs: ['City Girl', 'Fire and Bone', 'Boning Fire']
+      },
+      {
+        name: 'Big Country',
+        songs: ['City Girl', 'Fire and Bone', 'Boning Fire']
+      },
+      {
+        name: 'Big Country',
+        songs: ['City Girl', 'Fire and Bone', 'Boning Fire']
+      },
+    ]
+  },
 
 }
-
 
 class Filter extends Component {
   render() {
     return (
       <div style={{ defaultStyle }}>
-        <img  />
+        <img />
         <input type="text" />
       </div>
     )
@@ -21,9 +47,9 @@ class Filter extends Component {
 class Playlist extends Component {
   render() {
     return (
-      <div style={{ ...defaultStyle, width: '25%', display: 'inline-block'}}>
+      <div style={{ ...defaultStyle, width: '25%', display: 'inline-block' }}>
         <img />
-        <h3>Playlist Name Yup</h3>
+        <h3>Playlist Name</h3>
         <ul>
           <li>
             Song 1
@@ -45,7 +71,8 @@ class Aggregate extends Component {
     return (
       <div style={{ defaultStyle, width: '40%', color: '#ccc', display: "inline-block" }}>
         <h2>
-          Number Text
+          {this.props.playlists
+            && this.props.playlists.length} Text
         </h2>
       </div>
     )
@@ -53,17 +80,34 @@ class Aggregate extends Component {
 }
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = { serverData: {} }
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ serverData: fakeServerData });
+    }, 500);
+  }
   render() {
     return (
       <div className="App">
-        <h1 style={{ color: '#fff' }}>Title</h1>
-        <Aggregate />
-        <Aggregate />
-        <Filter />
-        <Playlist />
-        <Playlist />
-        <Playlist />
-        <Playlist />
+          {this.state.serverData.user ?
+            <div>
+              <h1 style={{ color: '#fff' }}>
+                {this.state.serverData.user.name}'s Playlists
+              </h1>}
+                <Aggregate playlists={this.state.serverData.user
+                  && this.state.serverData.user.playlists} />
+                <Aggregate playlists={this.state.serverData.user
+                  && this.state.serverData.user.playlists} />
+                <Filter />
+                <Playlist />
+                <Playlist />
+                <Playlist />
+                <Playlist />
+            </div> : <h1 style={{defaultStyle,'text-align':'center'}}>Loading...</h1>
+          }
       </div>
     )
   }
